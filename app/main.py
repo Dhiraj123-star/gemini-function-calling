d35c38dc4d000c4c323fea02650e0100fb6ce4ad
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-04-25 19:40:30 +0530

    feat(api): add signup endpoint with auto-login support

--- app/main.py
+++ app/main.py
@@ -1,10 +1,60 @@
-from fastapi import FastAPI
-from app.schemas import QueryRequest,QueryResponse
-from app.agent import run_agent 
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-app = FastAPI(title= "AI Tool Calling API")
+from app.schemas import QueryRequest, QueryResponse, LoginRequest, TokenResponse,SignupRequest
+from app.agent import run_agent
+from app.auth import authenticate_user, create_access_token, verify_token,create_user
 
-@app.post("/ask",response_model=QueryResponse)
-def ask_ai(request:QueryRequest):
-    result = run_agent(request.query)
-    return QueryResponse(response=result)
+app = FastAPI()
+
+security = HTTPBearer()
+
+
+# -----------------------------
+# AUTH DEPENDENCY
+# -----------------------------
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    payload = verify_token(token)
+
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    return payload
+
+# -----------
+# SIGNUP
+# -----------
+
+@app.post("/signup",response_model=TokenResponse)
+def signup(data: SignupRequest):
+    success = create_user(data.username,data.password)
+    if not success:
+        raise HTTPException(status_code=400,detail="Username already taken ")
+
+    # Auto login after signup
+    token = create_access_token({"sub":data.username})
+    return {"access_token":token}
+
+
+# -----------------------------
+# LOGIN ENDPOINT
+# -----------------------------
+@app.post("/login", response_model=TokenResponse)
+def login(data: LoginRequest):
+    user = authenticate_user(data.username, data.password)
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({"sub": user["username"]})
+    return {"access_token": token}
+
+
+# -----------------------------
+# PROTECTED AI ENDPOINT
+# -----------------------------
+@app.post("/ask", response_model=QueryResponse)
+def ask(data: QueryRequest, user=Depends(get_current_user)):
+    result = run_agent(data.query)
+    return {"response": result}
